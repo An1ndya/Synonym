00ab8427bf91e045babaaf72d5d749c3bd2f35cd
Anindya Biswas <anindya33046@gmail.com>
2020-10-12 02:49:08 +0600

    Update testsynonymlist.py
    test for getting synonym for pydictionary

--- testsynonymlist.py
+++ testsynonymlist.py
@@ -1,14 +1,14 @@
 from PyDictionary import PyDictionary
 dictionary=PyDictionary()
 
-word = "hasbbb"
+word = "austere"
 
 synlist = dictionary.synonym(word)
 #print(synlist)
 
-if isinstance(synlist, list): 
-    for i in synlist:
-       print(i) 
+#if isinstance(synlist, list): 
+for i in synlist:
+   print(i) 
 
 
 

a28cbf1f13298f240a116f4ab3731f69775d03c1
Anindya Biswas <anindya33046@gmail.com>
2020-10-13 23:56:21 +0600

    Update processJsonData.py

--- processJsonData.py
+++ processJsonData.py
@@ -5,5 +5,9 @@ import json
 with open('austere-json.txt', 'r') as file:
     data = file.read().replace('\n', '')
 parsed = json.loads(data)
-print(parsed)
-#print(json.dumps(parsed, indent=4))
+#print(parsed)
+parsedFormated = (json.dumps(parsed, indent=4))
+writeFile = open("austere-json-formatted.txt", "x")
+writeFile.write(parsedFormated)
+file.close()
+writeFile.close()

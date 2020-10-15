625d2bcfded75fd55dca5e638e812fbc17bb5a94
Anindya Biswas <anindya33046@gmail.com>
2020-10-15 06:48:15 +0600

    Update processJsonData.py

--- processJsonData.py
+++ processJsonData.py
@@ -1,13 +1,44 @@
+#test purpose ignore it
 import json
+import os.path
+from os import path
 
+word = 'prosaic'
+strpath = 'word-json/' + word + '.json'
+if path.exists(strpath):
+    with open(strpath, 'r') as file:
+        data = file.read().replace('\n', '')
+else:
+    print('false')
 
-
-with open('word-json/austere-json.txt', 'r') as file:
-    data = file.read().replace('\n', '')
 parsed = json.loads(data)
 #print(parsed)
+'''
 parsedFormated = (json.dumps(parsed, indent=4))
-writeFile = open("austere-json-formatted.txt", "x")
+writeFile = open(word + "-json-formatted.json", "x")
 writeFile.write(parsedFormated)
 file.close()
 writeFile.close()
+'''
+#print(parsed[0]['meta']['syns'])
+#synonym list'
+
+count = 0
+for synlist in parsed[0]['meta']['syns']:
+    for syn in synlist:
+        count=count+1
+        print(syn)
+print("synonym count: " + str(count))
+
+
+#More synonym list
+count = 0
+sseqList = parsed[0]['def'][0]['sseq']
+for sseq in sseqList:
+    for synList in sseq[0][1]['sim_list']:
+        for syn in synList:
+            count=count+1
+            print(syn['wd'])
+print("synonym count: " + str(count))
+print(synList)
+
